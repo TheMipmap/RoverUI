a93b8082e4b78c68166bfeb060f40aded8c75c7c
TheMipmap <morten-ks@hotmail.com>
2021-11-04 19:05:11 +0100

    Added stage 2 and 3 in the program

--- Human-robot_interaction.ino
+++ Human-robot_interaction.ino
@@ -12,15 +12,14 @@ Zumo32U4ButtonB buttonB;
 Zumo32U4ButtonC buttonC;
 
 int accCountsR = 0;
-int commandsSelected = 0; // 0 -> F, 1 -> backwards, 2 -> right, 3 -> left
-
+int commandSelected = 0; // 0 -> F, 1 -> backwards, 2 -> right, 3 -> left
+int speedSelected = 100; // A speed between 0 and 400
+int timeSelected = 1; // Seconds the command should run
 int actualStage = 0;
 
 
 void setup() {
   Serial.begin(9600);
-  lcd.clear();
-  
 }
 
 
@@ -32,14 +31,24 @@ void loop() {
         delay(200);
         }
 
-    // stage 1 --> Select speed and time
+    // stage 1 --> Select speed
         if (actualStage == 1) {
           stage1();
           delay(200);
         }
 
-    // stage 2 --> Run the program
-
+    // stage 2 --> Select time
+        if (actualStage == 2) {
+          stage2();
+          delay(200);
+        }
+    
+    // Stage 3 --> Run the program
+        if(actualStage == 3) {
+          stage3();
+          delay(200);
+          Serial.println("Im in stage 3!");
+        }
 
   
 }
@@ -54,22 +63,22 @@ void stage0() {
             // increment or decrement the selected command
        if (accCountsR > 100) {
           beep();
-          commandsSelected += 1;
+          commandSelected += 1;
           accCountsR -= 100;
-          if (commandsSelected > 3) commandsSelected = 0;
+          if (commandSelected > 3) commandSelected = 0;
        } else if (accCountsR < -100) {
         beep();
-        commandsSelected -= 1;
+        commandSelected -= 1;
         accCountsR += 100;
-          if (commandsSelected < 0) commandsSelected = 3;
+          if (commandSelected < 0) commandSelected = 3;
        }
 
-       Serial.println("Counts: " + String(accCountsR) + "\n" + "Command: " + String(commandsSelected));
+       Serial.println("Counts: " + String(accCountsR) + "\n" + "Command: " + String(commandSelected));
           
         //Show it on the screen
             //Title
             //In which command we are 
-        LCD_stage0(commandsSelected);
+        LCD_stage0(commandSelected);
 
         
         //Until button pressed?
@@ -109,18 +118,167 @@ void LCD_stage0(int command) {
   lcd.print(nameCommand);
 }
 
+void LCD_stage1(int speed) {
+  String nameSpeed;
+  switch (speed) {
+    case 50:
+    nameSpeed = "50";
+    break;
+    
+    case 100:
+    nameSpeed = "100";
+    break;
+    
+    case 150:
+    nameSpeed = "150";
+    break;
+ 
+    case 200:
+    nameSpeed = "200";
+    break;
+
+    case 250:
+    nameSpeed = "250";
+    break;
+
+    case 300:
+    nameSpeed = "300";
+    break;
+
+    case 350:
+    nameSpeed = "350";
+    break;
+
+    case 400:
+    nameSpeed = "400";
+    break;
+
+    default:
+    nameSpeed = "Error";
+  }
+  
+  lcd.clear();
+  lcd.print("Speed >");
+  lcd.gotoXY(0,1);
+  lcd.print(nameSpeed);
+}
+
+void LCD_stage2(int time) {
+  
+  String nameTime = String(time);
+
+  
+  if (time > 15 || time < 1) nameTime = "Error";
+  
+
+  
+  lcd.clear();
+  lcd.print("Time >");
+  lcd.gotoXY(0,1);
+  lcd.print(nameTime);
+}
+
 void beep() {
   buzzer.playNote(NOTE_A(4), 20, 15);
 }
 
 void stage1() {
           // read the encoders (same function)
-       lcd.clear();
-       lcd.print((String)commandsSelected);
+       readEncoderStage0();
+       
         // if it's larger or lower than threshold then
-            // increment or decrement the selected command  
+            // increment or decrement the selected speed  
+         if (accCountsR > 100) {
+           speedSelected += 50;
+           beep();
+           accCountsR -= 100;
+           if (speedSelected > 400) speedSelected = 50;
+         } else if (accCountsR < 100) {
+            speedSelected -= 50;
+            beep();
+            accCountsR += 100;
+            if (speedSelected < 0) speedSelected = 400;
+         }
 
         //Show the Speed on the screen
+        LCD_stage1(speedSelected);
+
+        //Until button pressed?
+            //if yes I jump to the next stage and store the command
+        if (buttonA.isPressed()) {
+          actualStage = 2;
+          buttonA.waitForPress();
+        }
+        
+}
+
+void stage2() {
+   //Read encoders
+   readEncoderStage0();
+   
+   //if it is larger or lower than threshold do 
+      //increment or decrement the selected time
+    if (accCountsR > 100) {
+        timeSelected += 1;
+        beep();
+        accCountsR -= 100;
+        if (timeSelected > 15) timeSelected = 1;
+    } else if (accCountsR < 100) {
+        timeSelected -= 1;
+        beep();
+        accCountsR += 100;
+        if (timeSelected < 1) timeSelected = 1;
+    }
+    
+   //Show the time  on the display
+    LCD_stage2(timeSelected);
+
+   //Until button is pressed, wait further
+    if (buttonA.isPressed()) {
+      actualStage = 3;
+      buttonA.waitForPress();
+    }
+}
+
+void stage3() {
+  // run the command
+
+  switch (commandSelected) {
+    case 0:
+    motors.setSpeeds(speedSelected,speedSelected);
+    delay(timeSelected);
+    motors.setSpeeds(0,0);
+    break;
+    
+    case 1:
+    motors.setSpeeds(-speedSelected,-speedSelected);
+    delay(timeSelected);
+    motors.setSpeeds(0,0);
+    break;
+
+    case 2:
+    motors.setSpeeds(-speedSelected,speedSelected);
+    delay(timeSelected);
+    motors.setSpeeds(0,0);
+    break;
+
+    case 3:
+    motors.setSpeeds(speedSelected,-speedSelected);
+    delay(timeSelected);
+    motors.setSpeeds(0,0);
+    break;
+
+    default:
+    Serial.println("Something went wrong in stage 3");
+    lcd.clear();
+    lcd.print("SomeTINK");
+    lcd.gotoXY(0,1);
+    lcd.print("WRONG!!!");
+  }
+
+  //Set the actualStage to 0, to reset the program
+  actualStage = 0;
+  
 }
 
 void readEncoderStage0() {
